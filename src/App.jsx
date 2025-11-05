748e489a46e552442aa640ab4dd5729b45262f8c
Shilpa Koorayil <shilpakoorayil@gmail.com>
2025-11-05 16:04:22 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+
+import './App.css'
+import Basic from './embedding-expression/Basic'
+
+function App() {
+
+
+//handleSubmit 
+//curley brace
+//{name}!<h1>
+<>
+
+{/* .....................embedding -expression--------------------*/}
+<Basic/>
+</>
+
+  return (
+    // <>fragments
+    //   <h2>SHILPA KOORAYIL</h2>
+    //   <h1>SHILPA KOORAYIL</h1>
+
+    // </>
+    //alaways return singleparent element
+    //
+    
+    <div>
+     <h2>sHILPA</h2> 
+     <img src="" alt="" />
+      
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/embedding-expression/Basic.jsx
@@ -0,0 +1,14 @@
+
+
+import React from 'react'
+
+const Basic = () => {
+    const name='ShilpaK';
+  return (
+    <div>
+      <h2>Hi {name}</h2>
+    </div>
+  )
+}
+
+export default Basic
